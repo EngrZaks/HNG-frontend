0a6aa6656642655ad6270b08304580823f779b3c
EngrZaks <abzakariyya@gmail.com>
2022-11-05 17:26:45 +0100

    style: styled contact page, feat: added font

--- stage1/src/components/contact/Contact.jsx
+++ stage1/src/components/contact/Contact.jsx
@@ -4,18 +4,63 @@ import "./contact.scss";
 function Contact() {
   return (
     <div className="contact">
-      <h1>contact Me</h1>
-      <p>Hi there, contact me to ask me about anything you have in mind.</p>
-      <form>
-        <label>First name</label>
-        <input placeholder="Enter your first name" />
-        <label>Last name</label>
-        <input placeholder="Enter your last name" />
-        <label>Email</label>
-        <input placeholder="yourname@email.com" />
-        <label>Message</label>
-        <textarea placeholder="Send me a message and I'll reply you as soon as possible..." />
-      </form>
+      <div className="contact-wrapper">
+        <form>
+          <h1>contact Me</h1>
+          <p className="contact-text">
+            Hi there, contact me to ask me about anything you have in mind.
+          </p>
+          <div className="name">
+            <div className="first">
+              <label htmlFor="first_name">First name</label>
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                placeholder="Enter your first name"
+              />
+            </div>
+            <div className="last">
+              <label htmlFor="last_name">Last name</label>
+              <input
+                type="text"
+                name="last_name"
+                id="last_name"
+                placeholder="Enter your last name"
+              />
+            </div>
+          </div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="yourname@email.com"
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+            rows={7}
+          />
+          <div className="check">
+            <label>
+              You agree to providing your data to DevZaks who may contact you.
+            </label>
+            <input
+              type="checkbox"
+              // value=""
+              defaultChecked
+              id="aggree"
+              name="agree"
+            />
+          </div>
+          <button type="submit" id="btn__submit">
+            Send message
+          </button>
+        </form>
+      </div>
       <Footer />
     </div>
   );
