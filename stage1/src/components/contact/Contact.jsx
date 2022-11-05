564ce87b9fddd5be7207ddd2371a20087b527b6e
EngrZaks <abzakariyya@gmail.com>
2022-11-05 14:37:21 +0100

    feat: added the contact component elements style: reduced link elements spacing and handled responsiveness for footer

--- stage1/src/components/contact/Contact.jsx
+++ stage1/src/components/contact/Contact.jsx
@@ -5,6 +5,17 @@ function Contact() {
   return (
     <div className="contact">
       <h1>contact Me</h1>
+      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      <form>
+        <label>First name</label>
+        <input placeholder="Enter your first name" />
+        <label>Last name</label>
+        <input placeholder="Enter your last name" />
+        <label>Email</label>
+        <input placeholder="yourname@email.com" />
+        <label>Message</label>
+        <textarea placeholder="Send me a message and I'll reply you as soon as possible..." />
+      </form>
       <Footer />
     </div>
   );
