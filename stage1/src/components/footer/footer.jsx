66ca1cb93121fd75b161d490650fe6940e21eb1b
EngrZaks <abzakariyya@gmail.com>
2022-11-05 13:12:00 +0100

    feat: added footer and styled it. refactor: added Links component to home style: restyle home to match the refactoring

--- stage1/src/components/footer/footer.jsx
+++ stage1/src/components/footer/footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./footer.scss";
+import zuri from "../../assets/zuri.svg";
+import ingressive from "../../assets/ingressive.svg";
 
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="footer">
+      <img src={zuri} alt="zuri" />
+      <p className="text">HNG Internship 9 Frontend Task</p>
+      <img src={ingressive} alt="ingressive" />
+    </div>
+  );
 }
 
 export default Footer;
--- stage1/src/components/home/Home.js
+++ stage1/src/components/home/Home.js
@@ -2,6 +2,7 @@ import "./home.scss";
 import image from "../../assets/zaks.jpg";
 import Footer from "../footer/footer";
 import { BsGithub, BsSlack } from "react-icons/bs";
+import Links from "./Links";
 function Home() {
   return (
     <div className="home">
@@ -12,71 +13,7 @@ function Home() {
         <p id="twitter">@Engr_zaks</p>
         <p id="slack">DevZaks</p>
       </section>
-
-      <section className="links">
-        <a
-          className="link"
-          id="btn__twitter"
-          href="https://twitter.com/Engr_zaks"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Twitter
-        </a>
-        <a
-          className="link"
-          id="btn__zuri"
-          href="https://training.zuri.team/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Zuri training
-        </a>
-        <a
-          className="link"
-          id="books"
-          href="http://books.zuri.team"
-          target="_blank"
-          rel="noreferrer"
-          title="this is where you find books about design and coding"
-        >
-          This is where you find books about design and coding
-        </a>
-        <a
-          className="link"
-          id="book__python"
-          href="https://books.zuri.team/python-for-beginners?ref_id=DevZaks"
-          target="_blank"
-          rel="noreferrer"
-          title="python for beginners book"
-        >
-          Buy python for beginners
-        </a>
-        <a
-          className="link"
-          id="pitch"
-          href="https://background.zuri.team"
-          target="_blank"
-          rel="noreferrer"
-          title="background check"
-        >
-          Checking the background of your coder before a gig has never been
-          easier
-        </a>
-        <a
-          className="link"
-          id="book__design"
-          href="https://books.zuri.team/design-rules"
-          target="_blank"
-          rel="noreferrer"
-          title="background check"
-        >
-          Grab the best design book in town for FREE
-        </a>
-        <a className="link" href="/contact">
-          Contact Me
-        </a>
-      </section>
+      <Links />
       <section className="icons">
         <a href="https://hng9.slack.com" target="_blank" rel="noreferrer">
           <BsSlack />
